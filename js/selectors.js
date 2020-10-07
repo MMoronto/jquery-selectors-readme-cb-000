5922d089ec9e4282853d53f280e5b61c68094c1a
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-10-07 15:05:24 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -7,11 +7,11 @@ function lastImageSelector() {
 }
 
 function ninjaBabySelector() {
-return $('#');
+return $('#baby-ninja');
 }
 
 function divSelector() {
-  
+  return $('.pics')
 }
 
 function firstListItem() {
